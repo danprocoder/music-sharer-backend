85b87490e79804b8378677f60458f41f831acd55
Daniel Austin <danielaustin718@gmail.com>
2019-02-16 13:56:41 +0100

    Changed user route to GET method [Delivers #164003243]

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const userController = new UserController();
 
 router.post('/api/user/add', userController.addNewUser);
 router.post('/api/user/auth', userController.auth);
-router.post('/api/user', userController.userData);
-router.post('/api/user/:username', userController.userData);
+router.get('/api/user', userController.userData);
+router.get('/api/user/:username', userController.userData);
 
 export default router;
