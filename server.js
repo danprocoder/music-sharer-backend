9c61273da99581821e0030416618ec4f732e5e97
Daniel Austin <danielaustin718@gmail.com>
2019-02-21 14:02:40 +0100

    Allows PATCH method for api request

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware to handle CORS and preflight requests.
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Method', 'GET, POST');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH');
   res.header('Access-Control-Allow-Headers', '*');
 
   if (req.method === 'OPTIONS') {
